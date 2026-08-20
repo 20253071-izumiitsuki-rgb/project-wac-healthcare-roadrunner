acadea97620b017d93d5881dc67c1499916e7589
izumiitsuki <20253071-izumiitsuki@hcs.ac.jp>
2026-08-20 14:31:22 +0900

    konntoro-ra

--- src/main/java/com/study/wac1/healthcare/time/timecontroller.java
+++ src/main/java/com/study/wac1/healthcare/time/timecontroller.java
@@ -1,5 +1,6 @@
 package com.study.wac1.healthcare.time;
 
+import com.example.healthcare.form.Record;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
