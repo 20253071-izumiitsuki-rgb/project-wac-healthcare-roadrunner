2bde30666f54e9073bbda927b6ac5f15f75ef352
izumiitsuki <20253071-izumiitsuki@hcs.ac.jp>
2026-08-20 14:01:00 +0900

    がんばった

---
+++ src/main/java/com/study/wac1/healthcare/time/timeRecord.java
@@ -0,0 +1,27 @@
+package com.study.wac1.healthcare.time;
+
+public class timeRecord {
+  private String date;
+  private Integer duration;
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public Integer getDuration() {
+    return duration;
+  }
+
+  public void setDuration(Integer duration) {
+    this.duration = duration;
+  }
+
+  public void setInterval(Integer interval) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setInterval'");
+  }
+}
---
+++ src/main/java/com/study/wac1/healthcare/time/timecontroller.java
@@ -0,0 +1,35 @@
+package com.study.wac1.healthcare.time;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class timecontroller {
+
+  private List<timeRecord> recordList = new ArrayList<>();
+
+  @GetMapping("/time")
+  public String showTimePage(Model model) {
+    // HTMLへこれまでの記録リストを渡す
+    model.addAttribute("records", recordList);
+    return "time";
+  }
+
+  @PostMapping("/time/add")
+  public String addRecord(@RequestParam("date") String date,
+      @RequestParam("interval") Integer interval) {
+    timeRecord newRecord = new timeRecord();
+    newRecord.setDate(date);
+    newRecord.setInterval(interval);
+    recordList.add(newRecord);
+
+    // 登録後、GETの /time へリダイレクトする
+    return "redirect:/time";
+  }
+}
