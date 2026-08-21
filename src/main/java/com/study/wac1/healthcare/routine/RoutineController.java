b0b316085173d554039ca6d8b29ffe18a1529f2c
takagieita <20253097-takagieita@hcs.ac.jp>
2026-08-21 11:11:12 +0900

    RutinControllerできない

---
+++ src/main/java/com/study/wac1/healthcare/routine/RoutineController.java
@@ -0,0 +1,66 @@
+package com.study.wac1.healthcare.routine;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+public class RoutineController {
+
+    // 時間と行動をセットにするクラス
+    public static class RoutineItem {
+        private String time;
+        private String action;
+
+        public RoutineItem(String time, String action) {
+            this.time = time;
+            this.action = action;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public String getAction() {
+            return action;
+        }
+    }
+
+    @GetMapping("/api/routine")
+    public List<RoutineItem> getRoutine(@RequestParam String shift) {
+        List<RoutineItem> list = new ArrayList<>();
+
+        // シフトごとに返すデータを分岐
+        if ("day".equals(shift)) {
+            list.add(new RoutineItem("07:30", "🍳 朝食 ＆ ビタミンサプリ摂取"));
+            list.add(new RoutineItem("09:00", "💼 業務開始"));
+            list.add(new RoutineItem("20:00", "🛁 入浴（湯船に浸かって疲労回復）"));
+            list.add(new RoutineItem("23:00", "🛌 就寝（質の良い睡眠へ）"));
+        } else if ("night".equals(shift)) {
+            list.add(new RoutineItem("12:00", "🥞 起床 ＆ 軽めの食事"));
+            list.add(new RoutineItem("16:00", "🌙 夜勤開始"));
+            list.add(new RoutineItem("24:00", "🍌 夜食（消化に良いもの）"));
+        } else if ("off-morning".equals(shift)) {
+            list.add(new RoutineItem("09:00", "🚗 夜勤終了・帰宅"));
+            list.add(new RoutineItem("10:30", "💤 仮眠（3時間程度にとどめる）"));
+            list.add(new RoutineItem("21:30", "🛌 早めの就寝で自律神経を整える"));
+        } else {
+            list.add(new RoutineItem("08:30", "🏃‍♂️ 軽い運動やストレッチ"));
+            list.add(new RoutineItem("12:30", "🥩 栄養バランスの良い食事"));
+            list.add(new RoutineItem("22:00", "📱 画面を消して入眠準備"));
+        }
+
+        return list;
+    }
+
+    @GetMapping("/routine")
+    public String index() {
+        return "forward:/index.html";
+    }
+
+}
