cf8c252f274bf5f93244d3e1f6aa108c943a5924
izumiitsuki <20253071-izumiitsuki@hcs.ac.jp>
2026-08-21 14:42:49 +0900

    けした

--- src/main/java/com/study/wac1/healthcare/routine/RoutineController.java
+++ src/main/java/com/study/wac1/healthcare/routine/RoutineController.java
@@ -40,22 +40,22 @@ public class RoutineController {
         List<RoutineItem> list = new ArrayList<>();
 
         if ("day".equals(shift)) {
-            list.add(new RoutineItem("07:30", "🍳 朝食 ＆ ビタミンサプリ摂取"));
-            list.add(new RoutineItem("09:00", "💼 業務開始"));
-            list.add(new RoutineItem("20:00", "🛁 入浴（湯船に浸かって疲労回復）"));
-            list.add(new RoutineItem("23:00", "🛌 就寝（質の良い睡眠へ）"));
+            list.add(new RoutineItem("07:30", " 朝食 ＆ ビタミンサプリ摂取"));
+            list.add(new RoutineItem("09:00", " 業務開始"));
+            list.add(new RoutineItem("20:00", " 入浴（湯船に浸かって疲労回復）"));
+            list.add(new RoutineItem("23:00", " 就寝（質の良い睡眠へ）"));
         } else if ("night".equals(shift)) {
-            list.add(new RoutineItem("12:00", "🥞 起床 ＆ 軽めの食事"));
-            list.add(new RoutineItem("16:00", "🌙 夜勤開始"));
-            list.add(new RoutineItem("24:00", "🍌 夜食（消化に良いもの）"));
+            list.add(new RoutineItem("12:00", " 起床 ＆ 軽めの食事"));
+            list.add(new RoutineItem("16:00", " 夜勤開始"));
+            list.add(new RoutineItem("24:00", " 夜食（消化に良いもの）"));
         } else if ("off-morning".equals(shift)) {
-            list.add(new RoutineItem("09:00", "🚗 夜勤終了・帰宅"));
-            list.add(new RoutineItem("10:30", "💤 仮眠（3時間程度にとどめる）"));
-            list.add(new RoutineItem("21:30", "🛌 早めの就寝で自律神経を整える"));
+            list.add(new RoutineItem("09:00", " 夜勤終了・帰宅"));
+            list.add(new RoutineItem("10:30", " 仮眠（3時間程度にとどめる）"));
+            list.add(new RoutineItem("21:30", " 早めの就寝で自律神経を整える"));
         } else {
-            list.add(new RoutineItem("08:30", "🏃‍♂️ 軽い運動やストレッチ"));
-            list.add(new RoutineItem("12:30", "🥩 栄養バランスの良い食事"));
-            list.add(new RoutineItem("22:00", "📱 画面を消して入眠準備"));
+            list.add(new RoutineItem("08:30", " 軽い運動やストレッチ"));
+            list.add(new RoutineItem("12:30", " 栄養バランスの良い食事"));
+            list.add(new RoutineItem("22:00", " 画面を消して入眠準備"));
         }
         return list;
     }
